4889dd273d6901bc54bc5924598c0eb1127658fd
thefirstblair <thanyatorn.kr@ku.th>
2021-08-01 03:21:54 +0700

    Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,76 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <router-view/>
+          <v-app-bar
+        color="deep-purple accent-4"
+        dark
+      >
+        <v-app-bar-nav-icon @click="drawer = true" ></v-app-bar-nav-icon>
+        <v-toolbar-title>TypeWriter</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item >
+              <v-list-item-icon >
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+              
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-crown</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Leaderboard</v-list-item-title>
+            </v-list-item>
+            
+            <v-list-item :to="{path: 'reward'}"> 
+              <v-list-item-icon>
+                <v-icon>mdi-gift</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Reward</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-table</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>History</v-list-item-title>
+            </v-list-item>
+            
+          </v-list-item-group>
+        </v-list>
+
+        <div 
+            class="Login"
+        >
+            <v-btn 
+                block
+            >
+            Login
+            </v-btn>
+        </div>
+      </v-navigation-drawer>
+      <v-container grid-list-xs>
+        <router-view/>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -49,7 +81,7 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer : false
   }),
 };
 </script>
