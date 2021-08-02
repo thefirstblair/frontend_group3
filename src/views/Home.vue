a9335fabb40c3d8c028f702ba8e7b7ea1026e5ef
thefirstblair <thanyatorn.kr@ku.th>
2021-08-03 04:50:27 +0700

    make play button link to /typing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,28 +5,41 @@
         :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
         src="https://www.teahub.io/photos/full/96-966323_minimalist-wallpapers-minimal-background.jpg"
       >
-        <v-container fill-height>
-          <v-row align="center" class="white--text" justify="center">
-            <v-col class="white--text text-center" cols="12" tag="h1">
-              <span class="font-weight-black" style="font-size: 250%">
-                Typewriter Online
-              </span>
+      <v-container fill-height>
+        <v-row
+          align="center"
+          class="white--text"
+          justify="center"
+        >
+          <v-col
+            class="white--text text-center"
+            cols="12"
+            tag="h1"
+          >
+            <span class="font-weight-black" style="font-size: 250%" >
+              Typewriter Online
+            </span>
 
-              <br />
+            <br>
 
-              <span class="font-weight-light" style="font-size: 100%">
-                text text text
-              </span>
-            </v-col>
+            <span class="font-weight-light" style="font-size: 100%">
+              text text text
+            </span>
 
-            <v-btn to="/typing" elevation="2" x-large> Play now </v-btn>
-          </v-row>
-        </v-container>
+          </v-col>
+
+          <v-btn elevation="2" x-large to="/typing">
+            Play now
+          </v-btn>
+        </v-row>
+      </v-container>
       </v-img>
     </v-row>
   </v-main>
 </template>
 
 <script>
-export default {};
+  export default {
+
+  }
 </script>
