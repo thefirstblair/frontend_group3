0cdaeb7c264ff55a4820fd81624c9cb94e3b5ddd
bigbigbig <krisada.m@ku.th>
2021-08-03 02:41:29 +0700

    edit typing page

--- src/views/Typing.vue
+++ src/views/Typing.vue
@@ -1,18 +1,58 @@
 <template>
   <div>
-    <div class="bg-white text-xl h-18 w-1/2 mt-4 mx-auto p-4">
-      <span v-for="data in dataCom" :key="data.id" ref="dataSpan">{{
+    <div
+      class="flex justify-center mt-4 h-24 text-center text-5xl font-semibold"
+    >
+      <span class="bg-yellow-300 w-28">
+        Time
+        {{ timer }}
+      </span>
+    </div>
+    <div class="bg-gray-700 text-red-50 text-xl h-18 w-1/2 mt-4 mx-auto p-4">
+      <span v-for="data in divWord" :key="data.id" ref="dataSpan">{{
         data
       }}</span>
     </div>
-    <div class="bg-blue-200 h-20 w-1/2 mt-4 mx-auto text-red-50">
+    <div class="h-20 w-1/2 mt-4 mx-auto text-red-50">
       <textarea
         type="text"
         class="bg-blue-200 text-black w-full h-full"
         @focus="startGame"
         @keypress="typing"
+        @keydown.delete="pressDelete"
+        @keydown.space="pressSpace"
         v-model="inputField"
+        ref="typeField"
       />
+    </div>
+
+    <div
+      class="
+        flex
+        justify-center
+        space-x-16
+        w-1/2
+        text-center text-4xl
+        mx-auto
+        my-8
+      "
+    >
+      <span class="bg-yellow-300 w-auto">
+        Raw WPM <br />
+        {{ grossWPM }}
+      </span>
+      <span class="bg-yellow-300 w-auto">
+        Adjust WPM <br />
+        {{ netWPM }}
+      </span>
+      <span class="bg-yellow-300 w-28">
+        Errors
+        {{ incorrected }}
+      </span>
+      <span class="bg-yellow-300 w-28">
+        Score
+        {{ score }}
+      </span>
     </div>
   </div>
 </template>
@@ -22,11 +62,21 @@ export default {
   data() {
     return {
       word: "test cab equal curl quick quake manic quad reach quark chord quart pace quill sack equip track squid sick quack squat poach quota eject",
+
+      wooord: "cab equal curl quick",
       wordArr: [],
       divWord: "click text field to type",
-      datatest: "init",
       inputField: "",
       currCursor: 0,
+      allTyped: 0,
+      incorrected: 0,
+      grossWPM: 0,
+      netWPM: 0,
+      timer: 0,
+      isTimeRunning: true,
+      limit: 10,
+      typed: 0,
+      score: 0,
     };
   },
   computed: {
@@ -36,8 +86,13 @@ export default {
   },
   methods: {
     startGame() {
-      console.log("focus");
       this.updateWord();
+      if (this.$refs.dataSpan[this.currCursor]) {
+        this.$refs.dataSpan[this.currCursor].classList.add(
+          "text-black",
+          "bg-white"
+        );
+      }
     },
     updateWord() {
       this.divWord = this.word;
@@ -45,33 +100,98 @@ export default {
     typing(event) {
       //   this.$refs.dataSpan.forEach((char, index) => {
       const spanTexts = this.$refs.dataSpan;
-      if (this.inputField == null) {
-        this.$refs.dataSpan[this.currCursor].classList.remove("text-green-500");
-        this.$refs.dataSpan[this.currCursor].classList.remove("text-red-500");
-        // if (this.$refs.dataSpan[index].innerText === " ") {
-        //   this.$refs.dataSpan[index].classList.add("text-green-500");
-        // }
-      } else if (event.key === spanTexts[this.currCursor].innerText) {
-        this.$refs.dataSpan[this.currCursor].classList.add("text-green-500");
-        this.$refs.dataSpan[this.currCursor].classList.remove("text-red-500");
-      } else {
-        this.$refs.dataSpan[this.currCursor].classList.remove("text-green-500");
-        this.$refs.dataSpan[this.currCursor].classList.add("text-red-500");
+
+      if (this.currCursor === 0) {
+        this.countUpTimer();
+        console.log("start");
       }
-      //   console.log(this.$refs.dataSpan[index]);
+      if (this.$refs.dataSpan[this.currCursor]) {
+        if (event.key === spanTexts[this.currCursor].innerText) {
+          this.$refs.dataSpan[this.currCursor].classList.add(
+            "text-green-800",
+            "bg-green-300"
+          );
+          this.$refs.dataSpan[this.currCursor].classList.remove(
+            "text-red-800",
+            "bg-red-300"
+          );
+        } else {
+          this.$refs.dataSpan[this.currCursor].classList.remove(
+            "text-green-800",
+            "bg-green-300"
+          );
+          this.$refs.dataSpan[this.currCursor].classList.add(
+            "text-red-800",
+            "bg-red-300"
+          );
+          this.incorrected++;
+        }
+      }
+      this.typed++;
       this.currCursor++;
-      //   });
-      //   this.dataCom.forEach((char, index) => {
-      //     if (typedChar == null) {
-      //       console.log("null");
-      //     } else if (typedChar === char) {
-      //       char = "1";
-      //       console.log("true");
-      //     }
-      //   });
+      this.allTyped++;
+      this.startGame();
     },
-    toggle() {
-      this.correct = !this.correct;
+    pressDelete() {
+      this.startGame();
+      if (this.currCursor > 0) {
+        this.$refs.dataSpan[this.currCursor - 1].classList.remove(
+          "text-green-800",
+          "bg-green-300",
+          "text-red-800",
+          "bg-red-300"
+        );
+        this.$refs.dataSpan[this.currCursor].classList.remove(
+          "text-black",
+          "bg-white"
+        );
+        console.log("delete");
+        this.currCursor--;
+      }
+    },
+    pressSpace() {
+      for (let i = 0; i <= this.typed; i++) {
+        this.$refs.dataSpan[this.currCursor - i].remove();
+      }
+      if (this.$refs.dataSpan[this.currCursor].innerText !== " ") {
+        for (let i = 0; i <= 10; i++) {
+          if (this.$refs.dataSpan[this.currCursor].innerText === "") {
+            break;
+          }
+          this.$refs.dataSpan[this.currCursor].remove();
+          this.currCursor++;
+        }
+      }
+      this.typed = 0;
+      this.inputField = "";
+      // this.$refs.typeField.focus;
+    },
+    calScore() {
+      this.grossWPM = (this.allTyped / 5 / (this.limit / 60)).toFixed(3);
+      this.netWPM = (
+        (this.allTyped / 5 - this.incorrected) /
+        (this.limit / 60)
+      ).toFixed(3);
+      this.score = (this.netWPM * 12.5).toFixed(3);
+      console.log("Raw WPM", this.grossWPM);
+      console.log("Raw WPM", this.netWPM);
+    },
+    countUpTimer() {
+      if (this.limit > this.timer) {
+        setTimeout(() => {
+          this.timer += 1;
+          this.countUpTimer();
+        }, 1000);
+      } else {
+        this.savedScore();
+      }
+    },
+    savedScore() {
+      console.log("saved");
+      console.log("time", this.timer);
+      console.log("type", this.allTyped);
+      console.log("incorr", this.incorrected);
+      this.calScore();
     },
   },
 };
